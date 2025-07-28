0bdaec93f03b972d35108e94fc7e844ba3116548
rahul.sharma@zopoxo.com <Rahul.Sharma@zopoxo.com>
2025-07-28 16:22:43 +0530

    removed tests

--- src/__tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import App from '../renderer/App';
-
-describe('App', () => {
-  it('should render', () => {
-    const { container } = render(<App />);
-    expect(container).toBeInTheDocument();
-  });
-});
